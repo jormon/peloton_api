b17a57d2dadbe2f44846a4a3469bbc62c8715e7a
Jordan Moncharmont <jormon@gmail.com>
2020-05-17 11:04:09 -0700

    fix fields usage

--- lib/peloton_api/records/performance_graph.rb
+++ lib/peloton_api/records/performance_graph.rb
@@ -1,7 +1,7 @@
 module PelotonAPI
   module Records
     class PerformanceGraph < PelotonObject
-      field :summaries, :average_summaries, :duration, :metrics
+      fields :summaries, :average_summaries, :duration, :metrics
     end
   end
 end
